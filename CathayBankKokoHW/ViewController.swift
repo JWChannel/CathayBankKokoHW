8bd114c55518e6cc2b49df5a422129544be89b9a
jw <jasonflowstep@gmail.com>
2024-09-28 18:24:16 +0800

    Initial Commit

---
+++ CathayBankKokoHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CathayBankKokoHW
+//
+//  Created by J W on 2024/9/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
