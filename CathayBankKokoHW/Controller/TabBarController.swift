c5b6a533737b2720631877cac19d4c8f83d2144c
jw <jasonflowstep@gmail.com>
2024-10-03 08:25:46 +0800

    fix: tab bar UIEdgeInsets

--- CathayBankKokoHW/Controller/TabBarController.swift
+++ CathayBankKokoHW/Controller/TabBarController.swift
@@ -30,13 +30,17 @@ private extension UITabBarController {
     }
     
     func setupTabBar() {
+        let insets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        
         let productVC = UIViewController()
         productVC.view.backgroundColor = .white
         productVC.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "icTabbarProductsOff"), tag: 0)
+        productVC.tabBarItem.imageInsets = insets
         
         let FriendsNavController = FriendsNavController()
         FriendsNavController.view.backgroundColor = .white
         FriendsNavController.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "icTabbarFriendsOff"), tag: 1)
+        FriendsNavController.tabBarItem.imageInsets = insets
         
         let homeVC = UIViewController()
         homeVC.view.backgroundColor = .white
@@ -45,10 +49,12 @@ private extension UITabBarController {
         let manageVC = UIViewController()
         manageVC.view.backgroundColor = .white
         manageVC.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "icTabbarManageOff"), tag: 3)
+        manageVC.tabBarItem.imageInsets = insets
         
         let settingsVC = UIViewController()
         settingsVC.view.backgroundColor = .white
         settingsVC.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "icTabbarSettingOff"), tag: 4)
+        settingsVC.tabBarItem.imageInsets = insets
         
         viewControllers = [productVC, FriendsNavController, homeVC, manageVC, settingsVC]
     }
